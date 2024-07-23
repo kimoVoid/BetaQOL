76c4f5d1be96e55b3e6264ab471d63a7d458fc06
kimoVoid <bordakmc@gmail.com>
2024-07-23 00:43:47 -0300

    FOV slider adjustment

--- client/src/main/java/me/kimovoid/betaqol/mixin/feature/fovslider/OptionsScreenMixin.java
+++ client/src/main/java/me/kimovoid/betaqol/mixin/feature/fovslider/OptionsScreenMixin.java
@@ -2,6 +2,7 @@ package me.kimovoid.betaqol.mixin.feature.fovslider;
 
 import me.kimovoid.betaqol.feature.fov.FOVOption;
 import me.kimovoid.betaqol.feature.fov.FOVOptionSlider;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.options.OptionsScreen;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,12 +13,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(OptionsScreen.class)
 public abstract class OptionsScreenMixin extends Screen {
 
+    /**
+     * I need to remind myself to re-order the
+     * entire options menu. It's a mess.
+     */
+
     @Inject(method = "init", at = @At("TAIL"))
     private void addFovButton(CallbackInfo ci) {
+        int y = FabricLoader.getInstance().isModLoaded("sound-categories") ? 0 : 24 * (5 >> 1);
         this.buttons.add(new FOVOptionSlider(
                 69,
                 this.width / 2 - 155 + 160,
-                this.height / 6 + 24 * (5 >> 1),
+                this.height / 6 + y,
                 FOVOption.getLocalizedFov(),
                 FOVOption.fov));
     }
